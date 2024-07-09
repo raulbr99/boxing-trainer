aa057fe9fd4e7310fa831fb85ba8a01b9ea743ce
raulbr99 <47525849+raulbr99@users.noreply.github.com>
2024-07-09 13:32:26 +0100

    Update Timer.tsx

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -44,7 +44,10 @@ const Timer: React.FC = () => {
   };
 
   return (
-    <div className="flex flex-col items-center justify-center">
+    <div className={`flex flex-col items-center justify-center ${seconds === 0 ? 'bg-red-500 min-h-screen' : ''} w-full h-full`}>
+      <h1 className="text-6xl font-bold">
+        {String(minutes).padStart(2, '0')}:{String(seconds % 60).padStart(2, '0')}
+      </h1>
       <div className="flex flex-row items-center justify-center gap-3">
         <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={addSeconds}>
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
@@ -63,16 +66,6 @@ const Timer: React.FC = () => {
           </svg>
         </button>
       </div>
-      <h1 className="text-6xl font-bold">
-        {String(minutes).padStart(2, '0')}:{String(seconds % 60).padStart(2, '0')}
-      </h1>
-      <button
-        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-        onClick={startCounter}
-        disabled={isRunning}
-      >
-        Start
-      </button>
     </div>
   );
 };
